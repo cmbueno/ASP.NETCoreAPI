73f2540b3b4f1e3713b4e02cc42db7da74c3ada7
cbueno <sendjobs55@gmail.com>
2024-03-05 10:54:25 -0500

    Add project files.

---
+++ CodePulse.API/Controllers/CategoriesController.cs
@@ -0,0 +1,66 @@
+﻿using CodePulse.API.Data;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using CodePulse.API.Repositories.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryRepository categoryRepository;
+
+        public CategoriesController(ICategoryRepository _categoryRepository)
+        {
+            categoryRepository = _categoryRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryRequestDto request)
+        {
+            //Map DTO to Domain Model
+            var category = new Category
+            {
+                Name = request.Name,
+                UrlHandle = request.UrlHandle
+            };
+
+            await categoryRepository.CreateAsync(category);
+
+            //Transfer Domain Model to DTO
+
+            var response = new CategoryDTO 
+            { 
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+            };            
+
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllCategories()
+        {
+
+            var caterogies = await categoryRepository.GetAllAsync();
+
+            // Map Domain model to DTO
+
+            var response = new List<CategoryDTO>();
+            foreach (var category in caterogies)
+            {
+                response.Add(new CategoryDTO
+                {
+                    Id = category.Id,
+                    Name = category.Name,
+                    UrlHandle = category.UrlHandle
+                });
+            }
+
+            return Ok(response);
+        }
+    }
+}
---
+++ CodePulse.API/Repositories/Implementation/CategoryRepository.cs
@@ -0,0 +1,29 @@
+﻿using CodePulse.API.Data;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Repositories.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodePulse.API.Repositories.Implementation
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public CategoryRepository(ApplicationDbContext _dbContext)
+        {
+            dbContext = _dbContext;
+        }
+
+        public async Task<Category> CreateAsync(Category category)
+        {
+            await dbContext.Categories.AddAsync(category);
+            await dbContext.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<IEnumerable<Category>> GetAllAsync()
+        {
+            return await dbContext.Categories.ToListAsync();
+        }
+    }
+}
